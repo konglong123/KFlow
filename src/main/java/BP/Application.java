94e906df902774e2704a0215ca4b1a4f62b040f7
konglong123 <17888821571@163.com>
2020-01-06 20:06:37 +0800

    统一启动类

--- src/main/java/BP/Application.java
+++ src/main/java/BP/Application.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
-public class KflowWebApplication {
+public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(KflowWebApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
