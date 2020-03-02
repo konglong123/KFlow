381147bb2e9bb936c73837a2aba180ac5c378b04
konglong123 <17888821571@163.com>
2020-03-02 09:51:23 +0800

    实现资源的管理，资源语义检索、资源负载展示、资源预定，

--- src/main/java/BP/Application.java
+++ src/main/java/BP/Application.java
@@ -1,11 +1,13 @@
 package BP;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
+@MapperScan("BP.springCloud.dao")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
